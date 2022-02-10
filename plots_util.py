6f1dd099b65fa183e60051b7c0621ab7ea40f1d9
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-02-09 22:22:31 -0500

    Update plots_util.py

--- plots_util.py
+++ plots_util.py
@@ -65,3 +65,22 @@ def plotlyhistograms(data, variables):
         fig.add_trace(go.Histogram(x=data[col], name=col), row=1, col=i+1 )
 
     fig.show()
+
+
+def OutlierDetection(data:pd.DataFrame, varaible:str, factor:float = 1.5):
+
+    '''
+    Dedect outliers based on Inter Quantile Range
+
+    Args:
+        data: DataFrame
+        varaible: varaible to detect outliers
+        factor: factor to detect outliers usgin the expresions Q3 + factor*IQR  
+                and Q3 - factor*IQR (Default factor=1.5)
+    '''
+
+    q1 = data[varaible].quantile(q=0.25)
+    q3 = data[varaible].quantile(q=0.75)
+    IQR = q3 - q1 
+
+    return data.loc[data[varaible] > q3 + factor*IQR, varaible]
