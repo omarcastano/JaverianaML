4decfe395770aaf334cf96d2cd49482e19c44e53
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-02-24 14:37:29 -0500

    Update data_util.py

--- data_util.py
+++ data_util.py
@@ -2,16 +2,14 @@ import numpy as np
 import pandas as pd
 
 #Generate artificial polinomial data
-def poly_data_generator(n=50):
- 
-    def make_data(N=N, err=0.8, rseed=1):
+def poly_data_generator(N=50):
+    err=0.8
+    rseed=1
     # randomly sample the data
-        rng = np.random.RandomState(rseed)
-        X = rng.rand(N, 1) ** 2
-        y = 10 - 1. / (X.ravel() + 0.1)
-        if err > 0:
-            y += err * rng.randn(N)
-        return X, y
-    X, y = make_data(N=n, rseed = 0)
+    rng = np.random.RandomState(rseed)
+    X = rng.rand(N, 1) ** 2
+    y = 10 - 1. / (X.ravel() + 0.1)
+    if err > 0:
+        y += err * rng.randn(N)
 
     return X, y
