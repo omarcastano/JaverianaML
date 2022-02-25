667d147f1f8937d56575e2de6d722d4258c40d2c
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-02-24 21:43:26 -0500

    Update model_util.py

--- model_util.py
+++ model_util.py
@@ -8,7 +8,7 @@ from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.pipeline import Pipeline
 
 #Implements polinomial regression
-def PolynomialRegression(estimator ,degree, X_train, X_test, y_train, y_test):
+def PolynomialRegression(estimator ,degree, X_train, X_test, y_train, y_test, print_coef=False):
     #Pipeline
     model  = Pipeline(steps=[
                          ('poly', PolynomialFeatures(degree=degree)),
@@ -23,9 +23,10 @@ def PolynomialRegression(estimator ,degree, X_train, X_test, y_train, y_test):
     plt.scatter(X_test, y_test)
     print('R2_train =', model.score(X_train, y_train).round(5))
     print('R2_test =', model.score(X_test, y_test).round(5))
-    
     print('RMSE_train =', np.sqrt(mean_squared_error(y_train ,model.predict(X_train))).round(5) )
     print('RMSE_test =', np.sqrt(mean_squared_error(y_test ,model.predict(X_test))).round(5) )
+    if print_coef:
+        print(model['lin_reg'].coef_)
     x = np.linspace(0,1).reshape(-1,1)
     plt.plot(x, model.predict(x), 'r')
     plt.xlim(-0.01, 1)
