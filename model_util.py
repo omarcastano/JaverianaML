9e41f46591e75dce74ccaf679c9ccc8722aa9380
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-03-17 11:24:21 -0500

    Update model_util.py

--- model_util.py
+++ model_util.py
@@ -6,6 +6,8 @@ from sklearn.linear_model import  LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.pipeline import Pipeline
+from sklearn.utils import shuffle
+from sklearn.model_selection import ShuffleSplit
 
 #Implements polinomial regression
 def PolynomialRegression(estimator ,degree, X_train, X_test, y_train, y_test, print_coef=False):
@@ -32,3 +34,39 @@ def PolynomialRegression(estimator ,degree, X_train, X_test, y_train, y_test, pr
     plt.xlim(-0.01, 1)
     plt.ylim(-0.1, 11)
  
+
+
+###Sigmoid function
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+
+def SGD_LogisticRegression(X, y, learning_rate:float=0.001, epochs:int=100, batch_size:int=10):
+
+    '''
+    Implements mini-batch greadient descent to optmizer binary_cross_entropy loss function
+
+    Args: 
+        learning_rate:learning rate  (float)
+        epochs: number of total epochs
+        batch_size: number of instances in each mini-batch
+
+    '''
+
+    m = X.shape[1]
+    W = np.random.random(size=m)
+    total_epochs = epochs
+    total_batches = X.shape[0]//batch_size
+
+    for epoch in range(total_epochs):
+        X, y = shuffle(X_train, y_train)
+
+        for i in range(total_batches):
+
+            x_batch = X[(batch_size*i):(batch_size*(i+1))] 
+            y_batch = y[(batch_size*i):(batch_size*(i+1))]
+
+            p = sigmoid(x_batch.dot(W))
+            grad = x_batch.T.dot(p-y_batch)
+            W = W - learning_rate*grad
+
+    return W
