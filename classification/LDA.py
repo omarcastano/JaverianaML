92a11a58717bd0b537b695b582341d3f0db324a6
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-03-24 14:19:29 -0500

    Update LDA.py

--- classification/LDA.py
+++ classification/LDA.py
@@ -18,7 +18,7 @@ def Gaussian_plot(DB, shared_variance=False):
     '''
     Density plot of P(petal_width|y=c) assuming Gaussin pdfs for each class
     '''
-    
+    fig, ax = plt.subplots(1,2,figsize=(20,7))
     iris = sns.load_dataset('iris')
     iris = iris.loc[iris.species != 'setosa', :]
     plt.figure(figsize=(15,7))
@@ -35,14 +35,16 @@ def Gaussian_plot(DB, shared_variance=False):
         std2 = std
 
     x1 = np.linspace(mu1 - 3*std1, mu1 + 3*std1, 100)
-    plt.fill_between(x1, stats.norm.pdf(x1, mu1, std1), alpha=0.5, label="virginica")
+    ax[0].fill_between(x1, stats.norm.pdf(x1, mu1, std1), alpha=0.5, label="virginica")
 
     x2 = np.linspace(mu2 - 3*std2, mu1 + 3*std2, 100)
-    plt.fill_between(x2, stats.norm.pdf(x2, mu2, std2), alpha=0.5, label="versicolor")
-    plt.vlines(DB,0,2.1)
-    plt.text(0.9,1.5, r'$P(X|C_1)$', fontsize=20)
-    plt.text(2.3,1.5, r'$P(X|C_2)$', fontsize=20)
-    plt.xlabel("petal_width")
-    plt.ylabel("Density")
-    plt.legend()
-    plt.show()
+    ax[0].fill_between(x2, stats.norm.pdf(x2, mu2, std2), alpha=0.5, label="versicolor")
+    sns.scatterplot(data=iris, x='petal_width', y='species', hue='species', ax=ax[1])
+
+    ax[0].vlines(DB,0,2.1)
+    ax[1].vlines(DB,0,1)
+    ax[0].text(0.9,1.5, r'$P(X|C_1)$', fontsize=20)
+    ax[0].text(2.3,1.5, r'$P(X|C_2)$', fontsize=20)
+    ax[0].set_xlabel("petal_width")
+    ax[0].set_ylabel("Density")
+    ax[0].legend()
