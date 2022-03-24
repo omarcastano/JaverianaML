aaecfa99b7ec964f887c03c0c21c6cd2748604c9
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-03-24 12:36:08 -0500

    Update LDA.py

--- classification/LDA.py
+++ classification/LDA.py
@@ -14,7 +14,7 @@ import scipy.stats as stats
 
 
 ##Normal distribution for iris dataset
-def Gaussian_plot(DB):
+def Gaussian_plot(DB, shared_variance=False):
     '''
     Density plot of P(petal_width|y=c) assuming Gaussin pdfs for each class
     '''
@@ -22,17 +22,23 @@ def Gaussian_plot(DB):
     iris = sns.load_dataset('iris')
     iris = iris.loc[iris.species != 'setosa', :]
     plt.figure(figsize=(15,7))
+    
     mu1 = iris.groupby('species')['petal_width'].mean()['virginica']
-    sdt1 = iris.groupby('species')['petal_width'].std()['virginica']
+    std1 = iris.groupby('species')['petal_width'].std()['virginica']
 
     mu2 = iris.groupby('species')['petal_width'].mean()['versicolor']
-    sdt2 = iris.groupby('species')['petal_width'].std()['versicolor']
+    std2 = iris.groupby('species')['petal_width'].std()['versicolor']
+    
+    if shared_variance:
+        std =np.sqrt( 0.5*std**2 +  0.5*std1**2 )
+        std1 = std
+        std2 = std
 
-    x1 = np.linspace(mu1 - 3*sdt1, mu1 + 3*sdt1, 100)
-    plt.fill_between(x1, stats.norm.pdf(x1, mu1, sdt1), alpha=0.5, label="virginica")
+    x1 = np.linspace(mu1 - 3*std1, mu1 + 3*std1, 100)
+    plt.fill_between(x1, stats.norm.pdf(x1, mu1, std1), alpha=0.5, label="virginica")
 
-    x2 = np.linspace(mu2 - 3*sdt2, mu1 + 3*sdt2, 100)
-    plt.fill_between(x2, stats.norm.pdf(x2, mu2, sdt2), alpha=0.5, label="versicolor")
+    x2 = np.linspace(mu2 - 3*std2, mu1 + 3*std2, 100)
+    plt.fill_between(x2, stats.norm.pdf(x2, mu2, std2), alpha=0.5, label="versicolor")
     plt.vlines(DB,0,2.1)
     plt.text(0.88,1.5, r'$P(X|C_1)$', fontsize=20)
     plt.text(2.1,1.5, r'$P(X|C_2)$', fontsize=20)
