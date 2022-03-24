33336a70fd8397a8fb40c83d2dc2749a2501bd8f
omarcastano <56597125+omarcastano@users.noreply.github.com>
2022-03-24 12:07:13 -0500

    Update LDA.py

--- classification/LDA.py
+++ classification/LDA.py
@@ -14,7 +14,11 @@ import scipy.stats as stats
 
 
 ##Normal distribution for iris dataset
-def Gaussian_plot(b):
+def Gaussian_plot(DB):
+    '''
+    Density plot of P(petal_width|y=c) assuming Gaussin pdfs for each class
+    '''
+    
     iris = sns.load_dataset('iris')
     iris = iris.loc[iris.species != 'setosa', :]
     plt.figure(figsize=(15,7))
@@ -29,7 +33,7 @@ def Gaussian_plot(b):
 
     x2 = np.linspace(mu2 - 3*sdt2, mu1 + 3*sdt2, 100)
     plt.fill_between(x2, stats.norm.pdf(x2, mu2, sdt2), alpha=0.5, label="versicolor")
-    plt.vlines(b,0,2.1)
+    plt.vlines(DB,0,2.1)
     plt.text(0.88,1.5, r'$P(X|C_1)$', fontsize=20)
     plt.text(2.1,1.5, r'$P(X|C_2)$', fontsize=20)
     plt.xlabel("petal_width")
