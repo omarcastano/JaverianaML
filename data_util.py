9fabe23dd58ef50caf6bc2f1d5d62efc1f96ba03
omarcastano <omar.castano@udea.edu.co>
2023-06-22 13:47:14 -0500

    changes

--- data_util.py
+++ data_util.py
@@ -1,14 +1,36 @@
 import numpy as np
 import pandas as pd
 
-#Generate artificial polinomial data
-def poly_data_generator(N=50):
-    err=0.8
-    rseed=1
+
+def poly_data_generator(N: int = 50):
+    """
+    Generate artificial polynomial data
+
+    Arguments:
+    ----------
+    N: int, default=50
+        Number of samples
+
+    Returns:
+    --------
+    X: numpy.ndarray
+        Input data
+    y: numpy.ndarray
+        Output data
+
+    Examples:
+    ---------
+    >>> X, y = poly_data_generator(N=50)
+    >>> print(X.shape)
+    (50, 1)
+    """
+
+    err = 0.8
+    rseed = 1
     # randomly sample the data
     rng = np.random.RandomState(rseed)
     X = rng.rand(N, 1) ** 2
-    y = 10 - 1. / (X.ravel() + 0.1)
+    y = 10 - 1.0 / (X.ravel() + 0.1)
     if err > 0:
         y += err * rng.randn(N)
 
